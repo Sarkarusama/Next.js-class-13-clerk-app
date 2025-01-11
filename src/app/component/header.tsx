1f3370168d152b1869be5e53eed2c858198ec73e
sarkarusama <sheikhusama8017@gmail.com>
2025-01-11 21:47:17 +0500

    update

--- src/app/component/header.tsx
+++ src/app/component/header.tsx
@@ -1,38 +1,31 @@
-import Link from 'next/link';
-import React from 'react';
-import { auth } from '@clerk/nextjs/server';
-import { UserButton } from '@clerk/nextjs';
+import Link from 'next/link'
+import React from 'react'
+
 
 const Header = () => {
-  const userId  = auth(); // Check user authentication state
-
+    
   return (
     <>
-      <nav className="bg-slate-800 flex items-center py-6 px-6 mb-4 justify-between">
-        <div className="flex items-center">
-          <Link href="/">
-            <div className="text-lg font-bold text-white uppercase">
-              Clerk App
-            </div>
-          </Link>
-        </div>
-        <div className="text-white items-center flex">
-          {!userId ? (
-            <>
-              <Link href="/sign-in" className="text-white hover:text-gray-300 mr-4">
-                Sign In
-              </Link>
-              <Link href="/sign-up" className="text-white hover:text-gray-300 mr-4">
-                Sign Up
-              </Link>
-            </>
-          ) : (
-            <UserButton afterSignOutUrl="/" />
-          )}
-        </div>
-      </nav>
+     <nav className="bg-slate-800 flex item-center py-6 px-6 mb-4 justify-between">
+      <div className='flex items-center'>
+        <Link href="/">
+           <div className="text-lg font-bold text-white uppercase">
+            Clerk App
+           </div>
+        </Link>
+      </div>
+      <div className='text-white items-center flex'>
+       
+            
+            <Link href="sign-in" className='text-white hover:text-gray-300 mr-4'>Sign In</Link>
+            <Link href="sign-up" className='text-white hover:text-gray-300 mr-4'>Sign Up</Link>
+          
+     
+        
+      </div>
+    </nav> 
     </>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
