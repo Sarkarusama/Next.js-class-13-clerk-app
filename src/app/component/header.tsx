a5eda8fd7bda8ec2995c6ea6ea44d5e9452b6e31
sarkarusama <sheikhusama8017@gmail.com>
2025-01-11 19:54:20 +0500

    push

---
+++ src/app/component/header.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import React from 'react'
+import { auth } from '@clerk/nextjs/server'
+import { UserButton } from '@clerk/nextjs'
+
+const Header = () => {
+    const {userId} :any = auth()
+  return (
+    <>
+     <nav className="bg-slate-800 flex item-center py-6 px-6 mb-4 justify-between">
+      <div className='flex items-center'>
+        <Link href="/">
+           <div className="text-lg font-bold text-white uppercase">
+            Clerk App
+           </div>
+        </Link>
+      </div>
+      <div className='text-white items-center flex'>
+        {!userId && (
+            <>
+            <Link href="sign-in" className='text-white hover:text-gray-300 mr-4'>Sign In</Link>
+            <Link href="sign-up" className='text-white hover:text-gray-300 mr-4'>Sign Up</Link>
+            </>
+        )}
+        <div className='flex justify-center '>
+            <UserButton afterSignOutUrl="/"/>
+        </div>
+      </div>
+    </nav> 
+    </>
+  )
+}
+
+export default Header
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+   <h1 className="text-2xl ml-auto font-bold mb-4">Welcome</h1>
+   <p className="mb-5">This is a demo site for the USAMA JAMEEL Next.js And Clerk.Go ahead and sign in or sign up! </p>
+   </>
+  );
+}
